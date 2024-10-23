3e1a23e2ffa9e566f6d8420582cf25a7b24e0cc8
pisces85317 <pisces85317@gmail.com>
2024-10-23 14:34:40 +0800

    加入專案檔案。

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BigPorject.Controllers
+{
+	public class ProductController : Controller
+	{
+		public IActionResult All()
+		{
+			return View();
+		}
+	}
+}
